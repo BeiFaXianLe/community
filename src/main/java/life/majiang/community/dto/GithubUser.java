7dbd001acf01122d6b1cb8cf672d703259c64756
beifaxianle <903266030@qq.com>
2019-08-29 09:55:15 +0800

    first

---
+++ src/main/java/life/majiang/community/dto/GithubUser.java
@@ -0,0 +1,35 @@
+package life.majiang.community.dto;
+
+/**
+ * @anthor zxy
+ * @date 2019/8/15 17:24
+ **/
+public class GithubUser {
+    private String name;
+    private Long id;
+    private String bio;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+}
